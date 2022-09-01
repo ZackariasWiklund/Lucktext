cd89c4814e46543cdee1a6b5ca9a7341ae35baa6
ZackariasWiklund <112564594+ZackariasWiklund@users.noreply.github.com>
2022-09-01 15:32:51 +0200

    Update Program.cs

--- Lucktext/Program.cs
+++ Lucktext/Program.cs
@@ -8,13 +8,12 @@ Console.WriteLine("Skriv ditt namn");
 name = Console.ReadLine();
 
 
-Console.WriteLine("Skriv in din favorit animekaraktär");
-favoritkaraktär = Console.ReadLine();
 
 Console.WriteLine("Skriv in din favorit anime");
 favoritserie = Console.ReadLine();
 
-
+Console.WriteLine("Skriv in din favorit animekaraktär från den animen");
+favoritkaraktär = Console.ReadLine();
 
 Console.WriteLine($"Ey {name}, tänk att ha ett lika litet liv som dig som kollar på anime, speciellt om du kollar på {favoritserie}");
 Console.WriteLine($"Du tror säkert till och med att {favoritkaraktär} finns på riktigt");
